9579396b002c8abf4bce13807d5394dd87db2867
Romain Pouclet <romain.pouclet@gmail.com>
2018-10-15 13:50:16 -0700

    Initial Commit

---
+++ SampleAppWithFunctionsFlag/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SampleAppWithFunctionsFlag
+//
+//  Created by Romain Pouclet on 2018-10-15.
+//  Copyright © 2018 Buddybuild. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
